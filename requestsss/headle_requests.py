26b4a3928e50fb32f8821afd50249bb0a23c6f82
xing-6464 <xing_6464@foxmail.com>
2022-12-21 00:12:01 +0800

    requests模块的使用

--- requestsss/headle_requests.py
+++ requestsss/headle_requests.py
@@ -1,12 +1,32 @@
 import requests
 
-url = 'http://httpbin.org/'
+# url = 'http://httpbin.org/'
 
-# get请求
-response = requests.get(url + 'ip')
-print(response.text)
+# # get请求
+# response = requests.get(url + 'ip')
+# print(response.text)
 
-# post请求
-data = {'name': 'imooc'}
-res = requests.post(url + 'post', data=data)
-print(res.text)
+# # post请求
+# data = {'name': 'imooc'}
+# res = requests.post(url + 'post', data=data)
+# print(res.text)
+
+# 代参数的get请求
+# data = {
+#     'key1': 'value1',
+#     'key2': 'value2'
+# }
+# res = requests.get('http://httpbin.org/get', params=data)
+# print(res.url)
+# print(res.headers)
+
+# 使用get请求获取图片数据
+# res = requests.get('https://www.imooc.com/static/img/index/logo2020.png')
+# with open('imooc.png', 'wb') as f:
+#     f.write(res.content)
+
+# 请求json数据
+# res = requests.get('http://httpbin.org/ip')
+# data = res.json()
+# print(data)
+# print(data['origin'])
