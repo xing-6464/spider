cceabb9a2fac07eb0e066a3d355bc13b9c4d0549
xing-6464 <xing_6464@foxmail.com>
2022-12-21 13:00:05 +0800

    re serch方法

--- resolving/reg.py
+++ resolving/reg.py
@@ -3,12 +3,15 @@ import re
 pattern = re.compile(r'\d+')
 
 # match
-# match是头部匹配
-m1 = pattern.match('one12twothree34four')
-print(m1) # None
+# match是头部位置开始匹配
+# m1 = pattern.match('one12twothree34four')
+# print(m1) # None
 
-m2 = pattern.match('one12twothree34four', 3, 10)
-# group查看匹配结果
-print(m2.group())
+# m2 = pattern.match('one12twothree34four', 3, 10)
+# # group查看匹配结果
+# print(m2.group())
 
+# search 是字符串任意位置开始匹配
+# m1 = pattern.search('one12twothree34four')
+# print(m1)
 
